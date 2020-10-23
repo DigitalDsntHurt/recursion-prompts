f40025da5c0e083bb66d9d8930415b37cd8481a6
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-10-22 19:14:00 -0700

    Implement factorial easily. Implement sum of array in just under 5hrs. phewf.

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,21 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) { return null; }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) { return 0; }
+  if (array.length === 1) {
+    return array[0];
+  }
+  return array[0] + sum(array.slice(1))
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -32,6 +42,7 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
 };
 
 // 7. Compute the exponent of a number.
