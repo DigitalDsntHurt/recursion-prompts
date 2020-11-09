866b553161121ff883ac0fc50443bc9738bf1daf
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-09 13:58:33 -0800

    Implement palindrome. Started modulo and multiply.

--- src/recursion.js
+++ src/recursion.js
@@ -134,6 +134,10 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase().split(' ').join('');
+  if (string.length < 2) { return true; }
+  if (string[0] !== string[string.length - 1]) { return false; }
+  return palindrome(string.slice(1, -1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
